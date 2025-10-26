b475f6e8918401baff754f1670100b5fb9e348b7
Adithya P <adithyaperiyasamy494@gmail.com>
2025-10-26 19:48:23 +0530

    feat: finished interview page's UI

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent";
+
+const page = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="Adithya" userId="user1" typr="generate" />
+    </>
+  );
+};
+
+export default page;
